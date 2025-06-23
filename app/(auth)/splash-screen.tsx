df920fdc06f1c7b1e30d0c3e7fe0c94e327575b0
Arne Kittel <arne.kittel@icloud.com>
2025-06-23 08:35:51 +0200

    added logo to splash-screen

--- app/(auth)/splash-screen.tsx
+++ app/(auth)/splash-screen.tsx
@@ -3,17 +3,35 @@ import { Text } from '@/components/ui/text';
 import { VStack } from '@/components/ui/vstack';
 import { SafeAreaView } from '@/components/ui/safe-area-view';
 import { Button, ButtonText } from '@/components/ui/button';
+import { Image } from '@/components/ui/image';
+
+import { useColorScheme } from '@/components/useColorScheme';
 import { useRouter } from 'expo-router';
 
 export default function SignIn() {
+  const colorScheme = useColorScheme();
+
+  const imageSource = require("@/assets/logos/snb_logo.png")
+
   return (
     <SafeAreaView className='flex-1 bg-white'>
       <VStack
         className='p-9 w-full items-center h-full justify-center'
         space='lg'>
           <VStack
-            className='w-full'
+            className='w-full items-center'
             space='lg'>
+              {/* {colorScheme === "dark" ? (
+                <Image source={{uri: '@/assets/logos/3x/Artboard 1@3.png'}} className="w-[219px] h-10" />
+              ) : (
+                <Image source={{uri: '@/assets/logos/3x/darkArtboard 1@3.png'}} className="w-[219px] h-10" />
+              )} */}
+            <Image
+              size='2xl'
+              source={imageSource}
+              alt='image'
+                />
+
             <Button
               className='w-full'
               variant='solid'
