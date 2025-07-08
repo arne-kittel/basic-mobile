64e457f287344c7840fa67123166db7bcd4ebf03
Arne Kittel <arne.kittel@icloud.com>
2025-07-08 06:21:24 +0200

    Resolved inital app loading issue

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,6 +30,27 @@ export const unstable_settings = {
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+
+  const [loaded, error] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    ...FontAwesome.font,
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <ClerkProvider tokenCache={tokenCache}>
       <RootLayoutNav />
@@ -46,7 +67,7 @@ function RootLayoutNav() {
   
   console.log('Clerk isLoaded:', isLoaded);
   console.log('Clerk isSignedIn:', isSignedIn);
-  
+
   return (
     <GluestackUIProvider mode={(colorScheme ?? "light") as "light" | "dark"}>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
